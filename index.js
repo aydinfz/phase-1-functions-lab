bb63db199829c78ade0546d5b0e06fdcd8323ca1
Zehra <aydin.fzehra@gmail.com>
2023-03-18 18:56:23 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const distanceFromHqInBlocks = (loc) => {
+  return Math.abs(42 - loc);
+};
+
+const distanceFromHqInFeet = (loc) => {
+  return Math.abs(42 - loc) * 264;
+};
+
+const distanceTravelledInFeet = (loc1, loc2) => {
+  return Math.abs(loc1 - loc2) * 264;
+};
+
+const calculatesFarePrice = (start, destination) => {
+  const feet = Math.abs(start - destination) * 264;
+  if (feet > 2500) {
+    return "cannot travel that far";
+  } else if (feet > 2000) {
+    return 25;
+  } else if (feet > 400) {
+    return ((feet - 400) * 2) / 100;
+  } else {
+    return 0;
+  }
+};
